ad24a379e3c52390cddab069271beb679e0f98b1
Akila Mendis <akila@oneit.com.au>
2023-02-21 18:23:22 +0530

    27. How to start & Stop Appium Server Programmatically using AppiumServiceBuilder

--- src/test/java/akila/Appium/AppiumBasics.java
+++ src/test/java/akila/Appium/AppiumBasics.java
@@ -1,5 +1,6 @@
 package akila.Appium;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -7,11 +8,24 @@ import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 public class AppiumBasics {
 	
 	@Test
 	public void AppiumTest() throws MalformedURLException {
+		
+		File mainJS = new File("C:\\Users\\Akila\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js");
+		
+		AppiumServiceBuilder serviceBuilder = new AppiumServiceBuilder();
+		serviceBuilder.withAppiumJS(mainJS);
+		serviceBuilder.withIPAddress("127.0.0.1");
+		serviceBuilder.usingPort(4723);
+		
+		AppiumDriverLocalService service = AppiumDriverLocalService.buildService(serviceBuilder);
+		
+		service.start();
 		
 		UiAutomator2Options options = new UiAutomator2Options();
 		options.setDeviceName("Pixel 4 API 30");
@@ -20,7 +34,13 @@ public class AppiumBasics {
 		
 		URL server = new URL("http://127.0.0.1:4723");
 		AndroidDriver driver = new AndroidDriver(server, options);
+		
+		
+		
+		
+		
 		driver.quit();
+		service.stop();
 	}
 
 }
